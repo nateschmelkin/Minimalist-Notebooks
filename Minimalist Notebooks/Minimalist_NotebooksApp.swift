e85130b2064c25d704fa9e36db6b65d55879fbec
nateschmelkin <nate@schmelkin.com>
2025-03-31 17:04:41 -0400

    Initial Commit

---
+++ Minimalist Notebooks/Minimalist_NotebooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minimalist_NotebooksApp.swift
+//  Minimalist Notebooks
+//
+//  Created by Nate Schmelkin on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Minimalist_NotebooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Minimalist NotebooksTests/Minimalist_NotebooksTests.swift
@@ -0,0 +1,17 @@
+//
+//  Minimalist_NotebooksTests.swift
+//  Minimalist NotebooksTests
+//
+//  Created by Nate Schmelkin on 3/31/25.
+//
+
+import Testing
+@testable import Minimalist_Notebooks
+
+struct Minimalist_NotebooksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
